3ac8145090e111e45367085bd9d4d5768e291516
부산광역시서부교육지원청 <@DESKTOP-R0BS7Q5>
2024-08-01 17:19:36 +0900

    1차 이동 ~~

---
+++ zoeJavaTest/src/main/java/com/example/demo/ZoeJavaTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZoeJavaTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZoeJavaTestApplication.class, args);
+	}
+
+}
